1bc5a3eddce7bf07bbe3bb658a687b3e8239632f
imam0321 <imam.hossain0321@gmail.com>
2025-05-27 21:49:50 +0600

    Refactor SQL queries for clarity and organization

--- groupBy.sql
+++ groupBy.sql
@@ -2,3 +2,25 @@ SELECT * FROM students;
 
 SELECT country FROM students
   GROUP BY country;
+
+SELECT sp.common_name, s.sighting_time, r.name
+FROM
+    sightings s
+    JOIN species sp ON s.species_id = sp.species_id
+    JOIN rangers r ON s.ranger_id = r.ranger_id
+LIMIT 2;
+
+SELECT
+    sighting_id,
+    CASE
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) < 12 THEN 'Morning'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 12 AND 17  THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
